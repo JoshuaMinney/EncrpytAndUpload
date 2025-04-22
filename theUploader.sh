12cbe03426a08928d6f20da9cf1abf102e2b5834
JoshuaMinney <Joshua2.Minney@live.uwe.ac.uk>
2025-04-22 15:32:32 +0100

    Update theUploader.sh
    fixed "file not recognized" bug

--- theUploader.sh
+++ theUploader.sh
@@ -8,7 +8,7 @@ curl -X POST https://content.dropboxapi.com/2/files/upload \
     --header "Authorization: Bearer $accesskey" \
     --header "Dropbox-API-Arg: {\"path\": \"/$uploadfilename\",\"mode\": \"add\",\"autorename\": true,\"mute\": false}" \
     --header "Content-Type: application/octet-stream" \
-    --data-binary @"$file"
+    --data-binary "$2"
 
 exit 0
 
