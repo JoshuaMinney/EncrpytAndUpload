a2db6ef06d91d0a6b04f2281acb214a40d5a4713
JoshuaMinney <Joshua2.Minney@live.uwe.ac.uk>
2025-04-22 14:50:43 +0100

    Update theEncryptor.sh
    fixed bug where doesnt shred properly

--- theEncryptor.sh
+++ theEncryptor.sh
@@ -1,24 +1,32 @@
+  GNU nano 4.8                                                                                    encryptor2.sh                                                                                    Modified  
 #!/bin/bash
 
 file="$1"
-echo "$1";
+echo "Processing $1"
+
+echo "Enter password: "
+read -s password
+
 recursiveENC()
 {
-	if   [ -d "$1" ]; then
-		echo "$1 is a directory";
-		for FILE in "$1"/*; do
-			if [ -d "$FILE" ]; then
-				recursiveENC "$FILE"
+        if   [ -d "$1" ]; then
+                echo "$1 is a directory";
+                for FILE in "$1"/*; do
+                        if [ -d "$FILE" ]; then
+                                recursiveENC "$FILE"
 
-			elif [ -f "$FILE" ]; then
-				echo "encrypting $FILE";
-				EXTENSION="${FILE##*.}" 
-				openssl enc -aes-256-cbc -in "$FILE" -out "$FILE" -k "password"
-				echo "shred -u "$FILE"";
-			fi
-		done
-	else echo "$1 is not valid";
-     		exit 1
-	fi
+                        elif [ -f "$FILE" ]; then
+                                echo "encrypting $FILE";
+                                newfile="${FILE}.enc"
+                                openssl enc -aes-256-cbc -in "$FILE" -out "$newfile" -k "$password"
+                                echo "origianl file deleted: $FILE" 
+                                shred -u "$FILE"
+                        fi
+                done
+        else echo "$1 is not a valid folder";
+                exit 1
+        fi
 }
 recursiveENC "$file"
+
+
