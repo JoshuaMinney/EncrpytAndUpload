1c0fd67a83de76f23a24b637f10584cd025da1e3
JoshuaMinney <Joshua2.Minney@live.uwe.ac.uk>
2025-04-30 13:04:47 +0100

    added comments

--- theUploader.sh
+++ theUploader.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+
+#this is necessary as i had an issue where because curl was outdated, it wouldnt upload. It must be up to date to run properly
 sudo apt remove curl
 sudo apt update
 sudo apt install curl
 
-uploadfilename="$1"
-file="$2"
-accesskey="$3"
+#variables per argument passed
+uploadfilename="$1" #name of the file on dropbox
+file="$2" #name of the file on the computer
+accesskey="$3" #access token for dropbox
 
 curl -X POST https://content.dropboxapi.com/2/files/upload \
     --header "Authorization: Bearer $accesskey" \
